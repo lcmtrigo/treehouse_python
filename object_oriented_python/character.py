2021503a96fe092722beb6078a09c06726738186
rmtrigo <rmontrigo@gmail.com>
2015-08-18 21:48:19 -0700

    5: Object Oriented Python - Earned Inheritance badge

---
+++ object_oriented_python/character.py
@@ -0,0 +1,39 @@
+import random
+
+from combat import Combat
+
+class Character(Combat):
+	attack_limit = 10
+	experience = 0
+	hit_points = 10
+
+	def attack(self):
+		roll = random.randint(1, self.attack_limit)
+		if self.weapon == 'sword':
+			roll += 1
+		elif self.weapon == 'axe':
+			roll += 2
+		return roll > 4
+
+	def get_weapon(self):
+		weapon_choice = input("Weapon ([S]word, [A]xe, [B]ow, [W]and): ").lower()
+
+		if weapon_choice in 'sabw':
+			if weapon_choice == 's':
+				return 'sword'
+			elif weapon_choice == 'a':
+				return 'axe'
+			elif weapon_choice == 'b':
+				return 'bow'
+			else:
+				return 'wand'
+		else:
+			return self.get_weapon()
+
+
+	def __init__(self, **kwargs):
+		self.name = input("Name: ")
+		self.weapon = self.get_weapon()
+
+		for key, value in kwargs.items():
+			setattr(self, key, value)
