914b6a555d8ad318d4704dda12e7d920567b1c47
rmtrigo <rmontrigo@gmail.com>
2015-08-19 21:04:53 -0700

    7: Data Science - Earned Getting Started with Data Science badge

---
+++ data_science_basics/src/s2v1.py
@@ -0,0 +1,30 @@
+import csv
+import numpy
+
+def open_with_csv(filename):
+	data = []
+	with open(filename, encoding='utf-8') as tsvin:
+		tie_reader = csv.reader(tsvin, delimiter='\t')
+		for line in tie_reader:
+			data.append(line)
+	return data
+
+data_from_csv = open_with_csv('data.csv')
+print(data_from_csv[0])
+
+FIELDNAMES = ['', 'id', 'priceLabel', 'name', 'brandId', 
+'brandName', 'imageLink', 'desc', 'vendor', 'patterned', 
+'material']
+
+DATATYPES = [('myint', 'i'), ('myid', 'i'), ('price', 'f8'),
+('name', 'a200'), ('brandId', '<i8'), ('brandName', 'a200'),
+('imageUrl', '|S500'), ('description', '|S900'), 
+('vendor', '|S100'), ('pattern', '|S50'), ('material', '|S50')]
+
+def load_data(filename, d='\t'):
+	my_csv = numpy.genfromtxt(filne, delimiter=d, skip_header=1,
+		invalid_raise=False, names=FIELDNAMES,
+		dtype=DATATYPES)
+	return my_csv
+
+my_csv = load_data('data.csv')
