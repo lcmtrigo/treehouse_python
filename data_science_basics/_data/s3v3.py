516b8569f3a2feaf248c6c6bcfc9586201ecd524
rmtrigo <rmontrigo@gmail.com>
2015-08-26 00:50:58 -0700

    9: Data Science - Earned Exporting badge

--- data_science_basics/_data/s3v3.py
+++ data_science_basics/_data/s3v3.py
@@ -7,22 +7,22 @@ max_gucci = find_max(gucci_ties[1:], 2)
 max_jcrew = find_max_min(jcrew_ties[1:], 2)
 
 message = "{} {} tie price is = ${:03.2f}"
-print(message.format("Maximum", "Gucci", max_gucci))
-print(message.format("Maximum", "J.Crew", max_jcrew))
+# print(message.format("Maximum", "Gucci", max_gucci))
+# print(message.format("Maximum", "J.Crew", max_jcrew))
 
-avg_gucci = find_average(gucci_ties, True)
-avg_jcrew = find_average(jcrew_ties, True)
-print(message.format("Average", "Gucci", avg_gucci))
-print(message.format("Average", "J.Crew", avg_jcrew))
+# avg_gucci = find_average(gucci_ties, True)
+# avg_jcrew = find_average(jcrew_ties, True)
+# print(message.format("Average", "Gucci", avg_gucci))
+# print(message.format("Average", "J.Crew", avg_jcrew))
 
-striped_ties = filter_col_by_string(data_from_csv, "print", "_striped")
-print_ties = filter_col_by_string(data_from_csv, "print", "_print")
-paisley_ties = filter_col_by_string(data_from_csv, "print", "_paisley")
-solid_ties = filter_col_by_string(data_from_csv, "print", "_solid")
+# striped_ties = filter_col_by_string(data_from_csv, "print", "_striped")
+# print_ties = filter_col_by_string(data_from_csv, "print", "_print")
+# paisley_ties = filter_col_by_string(data_from_csv, "print", "_paisley")
+# solid_ties = filter_col_by_string(data_from_csv, "print", "_solid")
 
-message2 = "{}\t${:03.2f}"
-print("Print\tAverage")
-print(message2.format("striped", find_average(striped_ties)))
-print(message2.format("print", find_average(print_ties)))
-print(message2.format("paisley", find_average(paisley_ties)))
-print(message2.format("solid", find_average(solid_ties)))
+# message2 = "{}\t${:03.2f}"
+# print("Print\tAverage")
+# print(message2.format("striped", find_average(striped_ties)))
+# print(message2.format("print", find_average(print_ties)))
+# print(message2.format("paisley", find_average(paisley_ties)))
+# print(message2.format("solid", find_average(solid_ties)))
